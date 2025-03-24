5c2adfa6ea29b3161b549366a2abc15bce01d660
Gabriel <gabrielhhollandnell@gmail.com>
2025-03-24 14:06:11 +0100

    Update GameOfLife.java
    Fiksa litt på koden din så den blir bedre værsego

--- GameOfLife.java
+++ GameOfLife.java
@@ -4,23 +4,23 @@ public class GameOfLife {
     public static void main(String[] args) {
         Scanner tastatur = new Scanner(System.in);
 
-        System.out.print("Hvor mange rader? ");
+        System.out.print("Hvor mange rader?: ");
         int rader = Integer.parseInt(tastatur.nextLine());
 
-        System.out.println("Hvor mange kolonner? ");
+        System.out.println("Hvor mange kolonner?: ");
         int kolonner = Integer.parseInt(tastatur.nextLine());     
 
         Verden verden = new Verden(rader, kolonner);
         verden.tegn();
 
-        System.out.println("\"Enter\" for å Fortsette, \"Space-Enter\" for å Stoppe" );
+        System.out.println("\"Enter\" for å Fortsette, \"Space-Enter\" for å Stoppe: " );
         String fortsettelse = tastatur.nextLine();
 
         while (fortsettelse == "") {
             verden.oppdatering();
             verden.tegn();
 
-            System.out.println("\"Enter\" for å Fortsette, \"Space-Enter\" for å Stoppe" );
+            System.out.println("\"Enter\" for å Fortsette, \"Space-Enter\" for å Stoppe: " );
             fortsettelse = tastatur.nextLine();
         }
     }
