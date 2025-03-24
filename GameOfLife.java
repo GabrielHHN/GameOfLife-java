3b03b07d0cb0080b922d05c7c0a98803f85a7537
Gabriel <gabrielhhollandnell@gmail.com>
2025-03-24 14:13:00 +0100

    Update GameOfLife.java
    Måtte fikse litt greier som æ fakka opp

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
 
 public class GameOfLife {
-    public static void main(String[] args) {
+
+ public static void main(String[] args) {
         Scanner tastatur = new Scanner(System.in);
 
         System.out.print("Hvor mange rader? ");
+        System.out.print("Hvor mange rader vil du ha?: ");
         int rader = Integer.parseInt(tastatur.nextLine());
 
         System.out.println("Hvor mange kolonner? ");
+        System.out.println("Hvor mange kolonner vil du ha?: ");
         int kolonner = Integer.parseInt(tastatur.nextLine());     
 
         Verden verden = new Verden(rader, kolonner);
         verden.tegn();
 
         System.out.println("\"Enter\" for å Fortsette, \"Space-Enter\" for å Stoppe" );
+        System.out.println("trykk \"Enter\" for å FORTSETTE, eller trykk \"Space-Enter\" for å STOPPE: " );
         String fortsettelse = tastatur.nextLine();
 
         while (fortsettelse == "") {
@@ -21,6 +25,7 @@ public class GameOfLife {
             verden.tegn();
 
             System.out.println("\"Enter\" for å Fortsette, \"Space-Enter\" for å Stoppe" );
+            System.out.println("trykk \"Enter\" for å FORTSETTE, eller trykk \"Space-Enter\" for å STOPPE: " );
             fortsettelse = tastatur.nextLine();
         }
     }
